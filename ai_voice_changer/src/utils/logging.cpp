7479a682d9f5e1bc211c965c0d86f77db54167de
MoyaMryia <MryiaMoya@outlook.com>
2026-07-12 16:50:32 +0800

    Removed Old

--- ai_voice_changer/src/utils/logging.cpp
+++
@@ -1,4 +0,0 @@
-// utils/logging.cpp
-// logging is header-only (macros in logging.hpp); this file exists so CMake
-// has a translation unit for the logging module and IDEs index it.
-#include "utils/logging.hpp"
